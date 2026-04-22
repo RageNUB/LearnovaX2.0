fe2121515615baff3bc2d196c82699a696d47a37
RageNUB <jirimon39@gmail.com>
2026-04-23 02:44:38 +0600

    login and logout system implemented with auth

---
+++ auth/logout.php
@@ -0,0 +1,14 @@
+<?php
+// ============================================
+// LearnovaX - Logout
+// auth/logout.php
+// ============================================
+require_once '../config/auth.php';
+
+// Destroy session
+$_SESSION = [];
+session_destroy();
+
+// Redirect to login
+header("Location: ../auth/login.php");
+exit();
